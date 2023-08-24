52d2973c57c7e41715470c61969d1dfba2cbda3e
sachin <sachin@02>
2023-08-24 19:53:33 +0530

    First draft of the Alien Invasion Game with some basic settings

--- Alien Invasion.py
+++ Alien Invasion.py
@@ -9,6 +9,10 @@ def run_game():
     screen=pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
 
+    #set backgroud color. Here we define the RGB value for the background color and store it in a 
+    # variable
+    bg_color=(230,230,230)
+
     # Start the main loop for the game.
 
     while True:
@@ -16,7 +20,11 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+        
+        # Redraw the screen during each pass through the loop, using fill command and predefined RGB
+        # values.
+        screen.fill(bg_color)
+        
         # Make the most recently drawn screen visible
         pygame.display.flip()
 
