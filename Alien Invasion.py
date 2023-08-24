d4237350cb297b0a607af95fd7ae41e89e7100f6
sachin <sachin@02>
2023-08-24 20:21:42 +0530

    added .vs file to the .gitignore error fixed inside the Alien Invasion file : to set_mode commands was to be given in the tuple format I accidently gave it directly. Figured the error by rolling back to previous commit

--- Alien Invasion.py
+++ Alien Invasion.py
@@ -9,7 +9,7 @@ def run_game():
     pygame.init()
     
     ai_setting=Setting()
-    screen=pygame.display.set_mode(ai_setting.screen_width,ai_setting.screen_height)
+    screen=pygame.display.set_mode((ai_setting.screen_width,ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")
     # Start the main loop for the game.
 
