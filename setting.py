b5c34a2c246df62d296b858aa485a256cd25e91e
sachin <sachin@02>
2023-08-24 19:51:32 +0530

    Created settings.py that contains some basic settings of the Alien Inavasion

---
+++ setting.py
@@ -0,0 +1,7 @@
+class Setting():
+    def __init__(self):
+        """ Initialize the game's setting."""
+        # Screen settings
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(230,230,230)
