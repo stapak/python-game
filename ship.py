a9f9e6df1eb3da2fc5d3683fd80a90150778d0ba
Sachin <sachinkamat2004@outlook.com>
2023-08-25 11:59:08 +0530

    Added ship image inside the images folder. And added image's path inside the ship.py file.

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class ship():
 
         # Load the ship image and get its rect.
 
-        self.image=pygame.image.load('')
+        self.image=pygame.image.load('images/ship.bmp')
         self.rect= self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
